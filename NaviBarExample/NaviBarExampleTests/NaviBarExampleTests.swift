13d35ca3a88fba270ec334db5ec8357b3b86a6a1
minjun kim <minzzzun@gmail.com>
2024-10-04 21:17:15 +0900

    예시코드 파일 추가

---
+++ NaviBarExample/NaviBarExampleTests/NaviBarExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NaviBarExampleTests.swift
+//  NaviBarExampleTests
+//
+//  Created by 김민준 on 10/4/24.
+//
+
+import Testing
+@testable import NaviBarExample
+
+struct NaviBarExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ TabbarExample/TabbarExampleTests/TabbarExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarExampleTests.swift
+//  TabbarExampleTests
+//
+//  Created by 김민준 on 10/4/24.
+//
+
+import Testing
+@testable import TabbarExample
+
+struct TabbarExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
