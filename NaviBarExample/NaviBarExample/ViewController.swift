e5ef6842cb2f5a4b18992acc550e3c3b24c7f9a5
minjun kim <minzzzun@gmail.com>
2024-11-08 22:50:05 +0900

    tabbar 예시 코드 추가

--- NaviBarExample/NaviBarExample/ViewController.swift
+++ NaviBarExample/NaviBarExample/ViewController.swift
@@ -1,8 +1,9 @@
-//
-//  ViewController.swift
-//  NaviBarExample
-//
-//  Created by 김민준 on 11/8/24.
-//
 
-import Foundation
+import UIKit
+
+class ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
