4a3cc78e1bcb5dd47558d551378e7cd8fe0c98a0
minjun kim <minzzzun@gmail.com>
2024-11-15 00:00:59 +0900

    [Feat] : CustomView 안에 View 추가

--- CustomViewEx/CustomViewEx/CustomView.swift
+++ CustomViewEx/CustomViewEx/CustomView.swift
@@ -4,14 +4,33 @@ import UIKit
 
 class CustomView : UIView {
     
+    let miniView : DetailView = {
+        let view = DetailView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-    
+        setUI()
         self.backgroundColor = .yellow
     }
     
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    func setUI(){
+        self.addSubview(miniView)
+        
+        NSLayoutConstraint.activate([
+            miniView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            miniView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            miniView.widthAnchor.constraint(equalToConstant: 50),
+            miniView.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
     }
 }
---
+++ CustomViewEx/CustomViewEx/DetailView.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class DetailView : UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .red
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
