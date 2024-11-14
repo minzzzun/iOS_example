b11473502de9868137075d253d68291eeae7a436
minjun kim <minzzzun@gmail.com>
2024-11-15 04:37:01 +0900

    [Feat] : UIPageControl 예시코드 추가

---
+++ UIPageControlEx/UIPageControlExTests/UIPageControlExTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIPageControlExTests.swift
+//  UIPageControlExTests
+//
+//  Created by 김민준 on 11/15/24.
+//
+
+import Testing
+@testable import UIPageControlEx
+
+struct UIPageControlExTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
