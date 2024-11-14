20e83f4f605d336ff2826fd87360d1bdede3841c
minjun kim <minzzzun@gmail.com>
2024-11-15 04:06:30 +0900

    [Feat] : UISwitch 예시코드 추가

---
+++ UISwitchEx/UISwitchExTests/UISwitchExTests.swift
@@ -0,0 +1,17 @@
+//
+//  UISwitchExTests.swift
+//  UISwitchExTests
+//
+//  Created by 김민준 on 11/15/24.
+//
+
+import Testing
+@testable import UISwitchEx
+
+struct UISwitchExTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
